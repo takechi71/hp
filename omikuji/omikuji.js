d1e908dade6f15e674e62b4c28ef1e36d712e7f4
takechi71 <s248727q@st.go.tuat.ac.jp>
2024-08-12 19:13:16 +0900

    Update omikuji.js

--- omikuji/omikuji.js
+++ omikuji/omikuji.js
@@ -1,1 +1,11 @@
+function omikuji_show() {
+	var omikuji = ["大吉", "吉", "中吉", "小吉", "末吉", "凶", "大凶"];
+    
+    var number = Math.random();
+    number = number * omikuji.length;
+    number = Math.floor(number);
+    var message = omikuji[number];
 
+    var object = document.getElementById("omikuji");
+    object.innerText = message;
+}
